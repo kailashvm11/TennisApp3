37593b46e9635f93a05d170db0c3128ae343cfc5
Rajesh <kailashvm@yahoo.com>
2021-05-17 13:53:25 +0200

    feat: include other scores which have not reached deuce

--- src/main/java/com/tennis/TennisGame.java
+++ src/main/java/com/tennis/TennisGame.java
@@ -21,6 +21,19 @@ public class TennisGame {
         scoreMap.put(new Scoreboard(0, 3), "LOVE FORTY");
         scoreMap.put(new Scoreboard(0, 4), "GAME PLAYER B");
         scoreMap.put(new Scoreboard(3, 3), "DEUCE");
+        scoreMap.put(new Scoreboard(1, 1), "FIFTEEN ALL");
+        scoreMap.put(new Scoreboard(1, 2), "FIFTEEN THIRTY");
+        scoreMap.put(new Scoreboard(1, 3), "FIFTEEN FORTY");
+        scoreMap.put(new Scoreboard(2, 1), "THIRTY FIFTEEN");
+        scoreMap.put(new Scoreboard(2, 2), "THIRTY ALL");
+        scoreMap.put(new Scoreboard(2, 3), "THIRTY FORTY");
+        scoreMap.put(new Scoreboard(3, 1), "FORTY FIFTEEN");
+        scoreMap.put(new Scoreboard(3, 2), "FORTY THIRTY");
+        scoreMap.put(new Scoreboard(4, 1), "GAME PLAYER A");
+        scoreMap.put(new Scoreboard(4, 2), "GAME PLAYER A");
+        scoreMap.put(new Scoreboard(1, 4), "GAME PLAYER B");
+        scoreMap.put(new Scoreboard(2, 4), "GAME PLAYER B");
+
 
     }
 
--- src/test/java/com/tennis/TennisGameTest.java
+++ src/test/java/com/tennis/TennisGameTest.java
@@ -97,6 +97,102 @@ public class TennisGameTest {
         assertEquals( "DEUCE", score );
     }
 
+    @Test
+    public void shouldReturnFifteenAll() {
+        String[] pointWinners = {"A", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "FIFTEEN ALL", score );
+    }
+
+    @Test
+    public void shouldReturnFifteenThirty() {
+        String[] pointWinners = {"A", "B", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "FIFTEEN THIRTY", score );
+    }
+
+    @Test
+    public void shouldReturnFifteenForty() {
+        String[] pointWinners = {"B","A", "B", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "FIFTEEN FORTY", score );
+    }
+
+    @Test
+    public void shouldReturnThirtyFifteen() {
+        String[] pointWinners = {"A", "B", "A"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "THIRTY FIFTEEN", score );
+    }
+
+    @Test
+    public void shouldReturnThirtyAll() {
+        String[] pointWinners = {"A", "B", "A", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "THIRTY ALL", score );
+    }
+
+    @Test
+    public void shouldReturnThirtyForty() {
+        String[] pointWinners = {"A", "B", "B", "A", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "THIRTY FORTY", score );
+    }
+
+    @Test
+    public void shouldReturnFortyFifteen() {
+        String[] pointWinners = {"A", "B", "A", "A"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "FORTY FIFTEEN", score );
+    }
+
+    @Test
+    public void shouldReturnFortyThirty() {
+        String[] pointWinners = {"A", "B", "A", "A", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "FORTY THIRTY", score );
+    }
+
+    @Test
+    public void shouldReturnGameForPlayerAWithFourPointsWonToOne() {
+        String[] pointWinners = {"A", "B", "A", "A", "A"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "GAME PLAYER A", score );
+    }
+
+    @Test
+    public void shouldReturnGameForPlayerAWithFourPointsWonToTwo() {
+        String[] pointWinners = {"A", "B", "A", "B", "A", "A"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "GAME PLAYER A", score );
+    }
+
+    @Test
+    public void shouldReturnGameForPlayerBWithFourPointsWonToOne() {
+        String[] pointWinners = {"A", "B", "B", "B", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "GAME PLAYER B", score );
+    }
+
+    @Test
+    public void shouldReturnGameForPlayerBWithFourPointsWonToTwo() {
+        String[] pointWinners = {"A", "B", "B", "B", "A", "B"};
+        playManyPoints(pointWinners);
+        String score = game.getScore();
+        assertEquals( "GAME PLAYER B", score );
+    }
+
     private void playManyPoints(String[] pointWinners) {
         for (String pointWinner : pointWinners) {
             game.nextPointWon(pointWinner);
